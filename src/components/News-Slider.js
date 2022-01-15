78952ad83513aaf36d47c4239e08c813f7ee13d4
xplix <xplix@users.noreply.github.com>
2022-01-15 16:40:15 +0100

    Bootstrap/Cards

--- src/components/News-Slider.js
+++ src/components/News-Slider.js
@@ -1,4 +1,5 @@
 import "../css/News-Slider.css";
+import { CardGroup, Card, Container, Row, Col } from "react-bootstrap";
 import React, { Component } from "react";
 import axios from "axios";
 
@@ -23,13 +24,24 @@ export default class NewsSlider extends Component {
 
   render() {
     return (
-      <div>
-        <ul>
-          {this.state.articles.map((article) => (
-            <li key={article.id}>{article.attributes.Title}</li>
-          ))}
-        </ul>
-      </div>
+      <Container>
+  <Row>
+  {this.state.articles.map((article) => (
+            
+    <Col> <Card key= {article.id}>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>{article.attributes.Title}</Card.Title>
+      <Card.Text>
+      {article.attributes.Description}
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  </Col>
+  ))}
+  </Row>
+</Container>
     );
+
   }
 }
