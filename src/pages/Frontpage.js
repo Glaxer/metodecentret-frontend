0c410b90d5a97812426dae59220a4bf19547129e
Glaxer <JacobKjaer@outlook.dk>
2022-01-16 22:21:43 +0100

    Added Method-slider and Themes-slider

--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import NewsSlider from "../components/News-Slider";
+import MethodSlider from "../components/Method-Slider";
+import ThemesSlider from "../components/Themes-Slider";
+import "../css/Frontpage.css";
 
 export default function Frontpage() {
   return (
-    <Container>
-      <div>
-        <h1>Vi realiserer potentialet i socialt arbejde</h1>
-        <p>
+    <div>
+      <Container className="front-container">
+        <h1 className="front-header">Vi realiserer potentialet i socialt arbejde</h1>
+        <p className="front-text">
           Metodecentret arbejder sammen med kommuner og regioner for at styrke
           kvaliteten af sociale indsatser til gavn for udsatte borgere og
           samfundet.
         </p>
-      </div>
+      </Container>
       <NewsSlider />
-      <div>Method Slider</div>
-      <div>Themes Slider</div>
-    </Container>
+      <MethodSlider />
+      <ThemesSlider />
+    </div>
   );
 }
